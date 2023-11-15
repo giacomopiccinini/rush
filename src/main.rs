8ae5169a85085afdda7aa84160760213724d9271
giacomopiccinini <piccinini.giacomo@gmail.com>
2023-11-15 09:25:50 +0100

    [FMT]

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,8 @@ fn main() {
 
         // Create all sub-directories i.e. recreate the directory structure
         for sub_directory in sub_directories.into_iter() {
-            create_dir_all(&target_path.join(&sub_directory)).expect("Failed to create sub-directory");
+            create_dir_all(&target_path.join(&sub_directory))
+                .expect("Failed to create sub-directory");
         }
     };
 
@@ -58,7 +59,9 @@ fn main() {
             .collect::<Vec<_>>()
     } else {
         // If it is a single file, we simply get its name and put it into a vector
-        vec![PathBuf::from(source_path.clone().file_name().expect("Can't read file"))]
+        vec![PathBuf::from(
+            &source_path.file_name().expect("Can't read file"),
+        )]
     };
 
     // Underdtand if the target path is a directory or not
@@ -89,7 +92,6 @@ fn main() {
                 copy(&source_path, &target_path.join(&file)).expect("Can't copy file");
             } else {
                 // If it is a directory copy all files recursively
-                //println!("{:?}", &source_path.join(&file).);
                 copy(&source_path.join(&file), &target_path.join(&file)).expect("Can't copy file");
             }
         } else {

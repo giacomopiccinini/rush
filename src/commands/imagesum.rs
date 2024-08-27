c603854f40abfdf470a1394f32ebf07dc85caedc
giacomopiccinini <piccinini.giacomo@gmail.com>
2024-08-27 08:57:47 +0200

    [FEAT] Improve parallelisation

--- src/commands/imagesum.rs
+++ src/commands/imagesum.rs
@@ -1,79 +1,74 @@
 use image::io::Reader as ImageReader;
+use rayon::prelude::*;
 use std::collections::HashSet;
 use std::path::PathBuf;
 use walkdir::WalkDir;
+use std::io;
 
 use crate::ImagesumArgs;
 
+// Execute the imagesum command
 pub fn execute(args: ImagesumArgs) {
-    // Convert to path object
     let target = PathBuf::from(&args.target);
+    let image_extensions: HashSet<_> = ["jpg", "jpeg", "png", "bmp", "gif", "tiff"]
+        .iter().map(|&s| s.to_lowercase()).collect();
 
-    // Init the image counter
-    let mut counter = 0;
-
-    // Init the set of unique dimensions
-    let mut dimensions: HashSet<(u32, u32)> = HashSet::new();
-
-    // Case where the target is a directory
-    if target.is_dir() {
-        // Iterate over the files in the directory
-        for path in WalkDir::new(&target).into_iter().filter_map(|e| e.ok()) {
-            // Get the path
-            let path = path.path();
-
-            if path.is_dir() {
-                continue;
-            }
-
-            // If the path has an extension
-            if let Some(ext) = path.extension() {
-                // If the extension is an image extension
-                if ["jpg", "jpeg", "png", "JPG", "bmp", "gif", "tiff"]
-                    .contains(&ext.to_str().unwrap())
-                {
-                    // Increment the image counter
-                    counter += 1;
-
-                    // Try to read the dimensions of the image
-                    match ImageReader::open(&path) {
-                        Ok(reader) => {
-                            if let Ok((width, height)) = reader.into_dimensions() {
-                                dimensions.insert((height, width));
-                            }
-                        }
-                        Err(e) => {
-                            println!("Failed to read dimensions of {:?}: {}", path, e);
-                        }
-                    }
-                }
-            }
-        }
-    } else {
-        if ["jpg", "jpeg", "png", "JPG", "bmp", "gif", "tiff"].contains(
-            &target
-                .extension()
-                .expect("Error with extension")
-                .to_str()
-                .unwrap(),
-        ) {
-            // Increment the image counter
-            counter += 1;
-
-            // Try to read the dimensions of the image
-            match ImageReader::open(&target) {
-                Ok(reader) => {
-                    if let Ok((width, height)) = reader.into_dimensions() {
-                        dimensions.insert((height, width));
-                    }
-                }
-                Err(e) => {
-                    println!("Failed to read dimensions of {:?}: {}", target, e);
-                }
-            }
-        }
+    if !target.is_dir() && !is_image_file(&target, &image_extensions) {
+        println!("Target is neither a directory nor an image file.");
+        return;
     }
+
+    let (counter, dimensions) = if target.is_dir() {
+        process_directory(&target, &image_extensions)
+    } else {
+        process_single_image(&target)
+    };
+
     // Print the results
     println!("Total images: {}", counter);
     println!("Unique (height, width) pairs: {:?}", dimensions);
 }
+
+// Check if the file is an image file
+fn is_image_file(path: &PathBuf, extensions: &HashSet<String>) -> bool {
+    path.extension()
+        .and_then(|ext| ext.to_str())
+        .map(|ext| extensions.contains(&ext.to_lowercase()))
+        .unwrap_or(false)
+}
+
+fn process_directory(target: &PathBuf, extensions: &HashSet<String>) -> (usize, HashSet<(u32, u32)>) {
+    let results: Vec<_> = WalkDir::new(target)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|entry| !entry.file_type().is_dir())
+        .filter(|entry| is_image_file(&entry.path().to_path_buf(), extensions))
+        .par_bridge()
+        .filter_map(|entry| process_image(entry.path()))
+        .collect();
+
+    let counter = results.len();
+    let dimensions: HashSet<_> = results.into_iter().collect();
+    (counter, dimensions)
+}
+
+// If image succesfully processed, return (1, {height, width})
+// If not, return (0, {})
+fn process_single_image(path: &PathBuf) -> (usize, HashSet<(u32, u32)>) {
+    process_image(path)
+        .map(|dims| (1, [dims].into_iter().collect()))
+        .unwrap_or((0, HashSet::new()))
+}
+
+/// Process a single image file and return its dimensions.
+fn process_image(path: &std::path::Path) -> Option<(u32, u32)> {
+    ImageReader::open(path)
+        .and_then(|reader| {
+            reader.into_dimensions()
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+        })
+        .map(|(width, height)| (height, width))
+        .ok()
+}
+
+
